bb88f58916fdf60c7819212d752de6c5e7e7e486
Tzara <tzarame@gmail.com>
2023-12-06 22:12:03 +0200

    Math lesson done

--- 1.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-a=1
-b=5
-c=4 
-x=(-b+math.sqrt(pow(b,2)-4*a*c))/2*a
-x=(-b+math.sqrt(pow(b,2)-4*a*c))/2*a
-print(x)
--- huy_math_hw.py
+++
@@ -1,8 +0,0 @@
-import this 
-import math
-a=1
-b=5
-c=4 
-x=-b+math.sqrt(b)
-
-print(x)
