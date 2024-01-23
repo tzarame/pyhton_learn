a364a333afa2f33b276419707f92df7371a56485
Tzara <tzarame@gmail.com>
2024-01-23 22:07:11 +0200

    starting

---
+++ Homework_0-1.py
@@ -0,0 +1,23 @@
+#inputs 
+
+#number_1 = input("Enter fist number: ")
+#number_1 = int(number_1)
+# number_2 = input("Enter second number: ")
+# number_2 = int(number_2)
+
+#debug 
+number_1 = 10
+number_2 = 5
+
+operator = input("Enter math operator, + - * / ")
+    if operator == "+":
+        result = number_1 + number_2
+    elif operator == "-":
+        result = number_1 - number_2
+    elif operator == "-":
+        result = number_1 * number_2
+    elif operator == "-":
+        result = number_1 / number_2
+    else:
+        result = "Пошел нахуй"
+print(result)
---
+++ old/HW_1-1
@@ -0,0 +1,19 @@
+#inputs 
+
+#number_1 = input("Enter fist number: ")
+#number_1 = int(number_1)
+# number_2 = input("Enter second number: ")
+# number_2 = int(number_2)
+
+#debug 
+number_1 = 10
+number_2 = 5
+
+
+operator = input("Enter math operator, + - * / "):
+    if operator == "+":
+        result = number_1 + number_2
+    elif operator == "-":
+        result = number_1 - number_2
+
+print(result)
